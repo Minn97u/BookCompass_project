28d636f4ace70f6b335d88a025c01bcaa282e1b2
[AI10]김민석 <phoves@naver.com>
2024-06-12 13:41:30 +0900

    fix: 로그인 기능 개선

--- server/passport/strategies/google.js
+++ server/passport/strategies/google.js
@@ -2,27 +2,39 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../../models/userSchema');
 
+const clientID = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+// clientID와 clientSecret이 제대로 설정되었는지 확인합니다.
+if (!clientID || !clientSecret) {
+    throw new Error('GOOGLE_CLIENT_ID와 GOOGLE_CLIENT_SECRET 환경 변수가 필요합니다.');
+  }
+
 const googleStrategy = new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/auth/google/callback'
+    clientID: clientID,
+    clientSecret: clientSecret,
+    callbackURL: 'http://localhost:3001/api/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
 
-        if (!user) {
-            user = new User({
-                googleId: profile.id,
-                name: profile.displayName,
-                email: profile.emails[0].value,
-                // 다른 필요한 필드들
-            });
-            await user.save();
-        }
-        if (user.region === null || user.favoriteAuthor === null) {
-            return done(null, user, { needAdditionalInfo: true });
-        }
+        if(!user) {
+            user = await User.findOne({ email: profile.emails[0].value });
+            if (user) {
+                user.googleId = profile.id;
+                await user.save();
+            } else {
+                user = new User({
+                    googleId: profile.id,
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    profilePic: profile.photos && profile.photos.length > 0 ? profile.photos[0].value : null,
+                    // 다른 필요한 필드들
+                });
+                await user.save();
+            }
+        } 
+
         return done(null, user);
     } catch (err) {
         return done(err, false);
